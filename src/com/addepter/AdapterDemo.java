4f77e8261a811537f4d9d036e110cbd511fc7477
jinggeaaa <15564641022@163.com>
2018-10-23 22:04:19 +0800

    修改了 adapterDemo

--- src/com/addepter/AdapterDemo.java
+++ src/com/addepter/AdapterDemo.java
@@ -14,5 +14,6 @@ public class AdapterDemo {
         audioPlayer.play("mp4","only you.mp4");
         audioPlayer.play("vlc","only you.vlc");
         audioPlayer.play("bhd","only you.bhd");
+        System.out.println(" git ti jiao hou de di yi ci ti  jiao ");
     }
 }
